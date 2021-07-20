dee77a5f548d752f9738af1473adb025a56235f8
Ivannovi Jordan <ijord001@sledge.cs.ucr.edu>
2021-07-20 16:56:49 -0700

    Modified required files to run Systemcalls

--- syscall.c
+++ syscall.c
@@ -105,7 +105,7 @@ extern int sys_write(void);
 extern int sys_uptime(void);
 extern int sys_debug(void);
 extern int sys_exitWithStatus(void);
-// extern int sys_waitpid(void);
+extern int sys_waitpid(void);
 
 
 static int (*syscalls[])(void) = {
@@ -132,7 +132,7 @@ static int (*syscalls[])(void) = {
 [SYS_close]   sys_close,
 [SYS_debug]   sys_debug,
 [SYS_exitWithStatus]   sys_exitWithStatus,
-// [SYS_waitpid]   sys_waitpid,
+[SYS_waitpid]   sys_waitpid,
 };
 
 void
--- sysproc.c
+++ sysproc.c
@@ -25,7 +25,7 @@ sys_wait(void)
 {
   int* status;
 
-  if(argptr(1, (void*)&status, sizeof(*status)) < 0)
+  if(argptr(0, (void*)&status, sizeof(*status)) < 0)
     return -1;
 
   return wait(status);      // Removed pointer from parameter to get rid of casting error.
@@ -100,26 +100,27 @@ sys_exitWithStatus(void){
   int status;
 
   if(argint(0, &status) < 0){
-    return 0;
+    return -1;
   }
-  return exitWithStatus(status);
+  exitWithStatus(status);
+  return 0;     //not reached
 
 }
 
-// int 
-// sys_waitpid(void){
-//   int pid;
-//   int* status;
-//   int options = 0;
+int 
+sys_waitpid(void){
+  int pid;
+  int* status;
+  int options = 0;
 
-//   if(argint(0, &pid) < 0){
-//     return -1;
-//   }
-//   if (argptr (0 , (void*)&status ,sizeof(*status))){
-//     return -1;
-//   }
-//   return waitpid(pid, status, options);
-// }
+  if(argint(0, &pid) < 0){
+    return -1;
+  }
+  if (argptr (1, (void*)&status ,sizeof(*status))){
+    return -1;
+  }
+  return waitpid(pid, status, options);
+}
 
 void
 sys_debug(void)
